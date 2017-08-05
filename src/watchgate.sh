605d77c0946235d10bfce4f5264d82080e95c9a7
Yang Li <git@netcrop.org>
2017-08-06 06:43:03 +0800

    2017-08-06

--- src/watchgate.sh
+++ src/watchgate.sh
@@ -1,6 +1,6 @@
 cmdlist='sed basename cat id cut bash man mktemp egrep date env mv chpasswd pwgen hostname sudo cp chmod ln chown rm sha1sum sha512sum gpg2 shred mkdir systemctl'
 unset Watchgate
-declare -A Watchgate
+declare -Ax Watchgate
 for cmd in $cmdlist;do
   i="$(which $cmd)"
   [[ X$i == X ]] && return
