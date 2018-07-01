03072a4f8f6c584f5ab3e7d8525b6899f6644ef0
Yang Li <git@netcrop.org>
2018-07-01 10:25:18 +0800

    2018-07-01

--- src/configure.sh
+++ src/configure.sh
@@ -21,97 +21,109 @@ watchgate.substitution()
   Watchgate[systemddir]=/lib/systemd/system/
   builtin source <(${Watchgate[cat]}<<-SUB
 
-watchgate.query()
+watchgate()
 {
 #  set -o xtrace
-  [[ -a ${Watchgate[prefix]}${Watchgate[queryscript]} ]] \
-    && ${Watchgate[sudo]} ${Watchgate[rm]} -f ${Watchgate[prefix]}${Watchgate[queryscript]}
-   ${Watchgate[sudo]} ${Watchgate[cat]}<<-QUERY> ${Watchgate[prefix]}${Watchgate[queryscript]}
-#!${Watchgate[env]} ${Watchgate[bash]}
-${Watchgate[queryscript]}()
-{
-#  set -o xtrace
-  local user=\\\${1:?[user][optional stored seed sec file]}
-  local seed=\\\${2:-"${Watchgate[configdir]}${Watchgate[seedprefix]}"}
-  seed=\\\${seed%.asc}
-  if [[ -r \\\${2} || -a \\\$seed && -r \\\$seed.asc ]];then
-    local tmpfile=\\\$(${Watchgate[mktemp]})
-    builtin trap "${Watchgate[shred]} -u \\\$tmpfile" SIGHUP SIGTERM SIGINT
-    builtin declare -x GPG_TTY="\\\$(${Watchgate[tty]})"
-    ${Watchgate[gpg]} --homedir \\\$HOME/.gnupg --no-tty --decrypt --no-verbose --quiet \\\$seed.asc >\\\$tmpfile
-    if [[ \\\$? != 0 ]];then
-      builtin printf "Try using same gpg-agent and tty to query password.\n"
+  local user=\${1:?[user][optional: stored seed asc file]}
+  local seed=\${2:-"${Watchgate[configdir]}${Watchgate[seedprefix]}"}
+  seed=\${seed%.asc}
+  if [[ -r \${2} || -a \$seed && -r \$seed.asc ]];then
+    local tmpfile=\$(${Watchgate[mktemp]})
+    builtin trap "${Watchgate[shred]} -fu \$tmpfile" SIGHUP SIGTERM SIGINT
+    builtin declare -x GPG_TTY="\$(${Watchgate[tty]})"
+    ${Watchgate[gpg]} --homedir \$HOME/.gnupg --no-tty \
+    --decrypt --no-verbose --quiet \$seed.asc >\$tmpfile
+    if [[ \$? != 0 ]];then
+      builtin printf "User \${USER} needs to become the owner of \${GPG_TTY}.\n"
       return
     fi
     ${Watchgate[pwgen]} --capitalize --numerals --num-passwords=1 \
-      --secure --sha1=\\\$tmpfile#"\\\$user\\\$(${Watchgate[date]} +"%Y%m%d%H%M")" 8
-    ${Watchgate[shred]} -u \\\$tmpfile
+      --secure --sha1=\$tmpfile#"\$user\$(${Watchgate[date]} +"%Y%m%d%H%M")" 8
+    ${Watchgate[shred]} -fu \$tmpfile
     return
   fi
   builtin printf "Seed missing!\n"
   ${Watchgate[pwgen]} --capitalize --numerals --num-passwords=1 \
-    --secure --sha1=/dev/null#"\\\$user\\\$(${Watchgate[date]} +"%Y%m%d%H%M")" 8
+    --secure --sha1=/dev/null#"\$user\$(${Watchgate[date]} +"%Y%m%d%H%M")" 8
 #  set +o xtrace
 }
-${Watchgate[queryscript]} "\\\$@"
-QUERY
-  ${Watchgate[sudo]} ${Watchgate[chmod]} u=rx,g=rx,o= ${Watchgate[prefix]}${Watchgate[queryscript]}
-  ${Watchgate[sudo]} ${Watchgate[chown]} root:users ${Watchgate[prefix]}${Watchgate[queryscript]}
-#  set +o xtrace
+watchgate.query()
+{
+  local fun='watchgate'
+  local script="${Watchgate[prefix]}/\${fun}"
+  \builtin type -t \${fun} || return
+  ${Watchgate[rm]} -f \${script}
+  ${Watchgate[cat]} <<-WATCHGATEQUERY > \${script}
+#!${Watchgate[env]} ${Watchgate[bash]}
+\$(\builtin declare -f \${fun})
+\${fun} "\\\$@"
+WATCHGATEQUERY
+  ${Watchgate[chmod]} gu=rx,o= \${script}
+  ${Watchgate[chown]} $USER:users \${script}
+  \builtin unset -f \${fun}
 }
 watchgate.cron()
 {
-  local excludeuser=\${1:?[exclude user]}
-  [[ -a ${Watchgate[prefix]}${Watchgate[cronscript]} ]] \
-    && ${Watchgate[sudo]} ${Watchgate[rm]} -f ${Watchgate[prefix]}${Watchgate[cronscript]}
-  ${Watchgate[sudo]} ${Watchgate[cat]}<<-CRON> ${Watchgate[prefix]}${Watchgate[cronscript]}
-#!${Watchgate[env]} ${Watchgate[bash]}
-${Watchgate[cronscript]}()
-{
 #set -o xtrace
-  [[ \\\$(${Watchgate[id]} -u) != 0 ]] && return
+  [[ \$(${Watchgate[id]} -u) != 0 ]] && return
   local seed="${Watchgate[configdir]}${Watchgate[seedprefix]}"
-  if [[ ! -r \\\$seed || ! -r \\\$seed.asc ]];then
+  local excludeuser=\${1:?[exclude user]}
+  if [[ ! -r \$seed || ! -r \$seed.asc ]];then
     seed=/dev/null
     builtin printf "Seed missing!\n"
   fi
-  local tmpfile=\\\$(${Watchgate[mktemp]})
-  builtin trap "${Watchgate[shred]} -fu \\\$tmpfile" SIGHUP SIGTERM SIGINT
-  declare -a Users=(\\\$(${Watchgate[cut]} -d':' -f1,3 /etc/passwd|\
-    ${Watchgate[egrep]} ":[[:digit:]]{4}|:0"|\
+  local tmpfile=\$(${Watchgate[mktemp]})
+  builtin trap "${Watchgate[shred]} -fu \$tmpfile" SIGHUP SIGTERM SIGINT
+  declare -a Users=(\$(${Watchgate[cut]} -d':' -f1,7 /etc/passwd|\
+    ${Watchgate[egrep]} -v "nologin"|\
     ${Watchgate[cut]} -d':' -f1|\
     ${Watchgate[egrep]} -v \${excludeuser}))
   local i user word timestamp
-  timestamp=\\\$(${Watchgate[date]} +"%Y%m%d%H%M")
-  for user in \\\${Users[@]};do
-    builtin printf "\\\$user:" >>\\\$tmpfile
+  timestamp=\$(${Watchgate[date]} +"%Y%m%d%H%M")
+  for user in \${Users[@]};do
+    builtin printf "\$user:" >>\$tmpfile
     ${Watchgate[pwgen]} --capitalize --numerals \
-      --num-passwords=1 --secure --sha1=\\\$seed#\\\$user\\\$timestamp 8 >>\\\$tmpfile
+      --num-passwords=1 --secure --sha1=\$seed#\$user\$timestamp 8 >>\$tmpfile
   done
-  ${Watchgate[chpasswd]} <\\\$tmpfile
-  ${Watchgate[shred]} -fu \\\$tmpfile
+  ${Watchgate[chpasswd]} <\$tmpfile
+  ${Watchgate[shred]} -fu \$tmpfile
 #set +o xtrace
 }
-${Watchgate[cronscript]}
-CRON
-  ${Watchgate[sudo]} ${Watchgate[chmod]} u=rx,go= ${Watchgate[prefix]}${Watchgate[cronscript]}
-  ${Watchgate[sudo]} ${Watchgate[chown]} root:users ${Watchgate[prefix]}${Watchgate[cronscript]}
+watchgate.cron.install()
+{
+  local excludeuser=\${1:?[exlucde user]}
+  local fun='watchgate.cron'
+  local script="${Watchgate[prefix]}/\${fun}"
+  \builtin type -t \${fun} || return
+  ${Watchgate[rm]} -f \${script}
+  ${Watchgate[cat]} <<-WATCHGATECRONINSTALL > \${script}
+#!${Watchgate[env]} ${Watchgate[bash]}
+\$(\builtin declare -f \${fun})
+\${fun} "\${excludeuser}"
+WATCHGATECRONINSTALL
+  ${Watchgate[sudo]} ${Watchgate[chmod]} u=rx,go= \${script}
+  ${Watchgate[sudo]} ${Watchgate[chown]} root:users \${script}
+  \builtin unset -f \${fun}
 }
 watchgate.install()
 {
-  local prefix=
+  local prefix
   [[ \$(${Watchgate[basename]} \${PWD}) == watchgate ]] && prefix='src/'
   watchgate.uninstall
-  watchgate.cron \${1:?[exclude user]}
+  watchgate.cron.install \${1:?[exclude user]}
   watchgate.query
   ${Watchgate[sudo]} ${Watchgate[mkdir]} -p ${Watchgate[mandir]}
   ${Watchgate[sudo]} ${Watchgate[chmod]} 0755 ${Watchgate[mandir]}
-  ${Watchgate[sudo]} ${Watchgate[cp]} \${prefix}watchgate.1 ${Watchgate[mandir]}/watchgate.1
+  ${Watchgate[sudo]} ${Watchgate[cp]} \${prefix}watchgate.1 \
+  ${Watchgate[mandir]}/watchgate.1
   ${Watchgate[sudo]} ${Watchgate[chmod]} 0644 ${Watchgate[mandir]}/watchgate.1 
   ${Watchgate[sudo]} ${Watchgate[chown]} $USER:users ${Watchgate[mandir]}/watchgate.1
-  ${Watchgate[sudo]} ${Watchgate[cp]} \${prefix}watchgate.service ${Watchgate[systemddir]}/watchgate.service
-  ${Watchgate[sudo]} ${Watchgate[chmod]} 0644 ${Watchgate[systemddir]}/watchgate.service
-  ${Watchgate[sudo]} ${Watchgate[cp]} \${prefix}watchgate.timer ${Watchgate[systemddir]}/watchgate.timer
+  ${Watchgate[sudo]} ${Watchgate[cp]} \${prefix}watchgate.service \
+  ${Watchgate[systemddir]}/watchgate.service
+  ${Watchgate[sudo]} ${Watchgate[chmod]} 0644 \
+  ${Watchgate[systemddir]}/watchgate.service
+  ${Watchgate[sudo]} ${Watchgate[cp]} \${prefix}watchgate.timer \
+  ${Watchgate[systemddir]}/watchgate.timer
   ${Watchgate[sudo]} ${Watchgate[chmod]} 0644 ${Watchgate[systemddir]}/watchgate.timer
   ${Watchgate[sudo]} ${Watchgate[ln]} -s ${Watchgate[systemddir]}/watchgate.timer \
      ${Watchgate[systemddir]}/timers.target.wants/watchgate.timer
@@ -120,7 +132,8 @@ watchgate.uninstall()
 {
   ${Watchgate[sudo]} ${Watchgate[rm]} -f ${Watchgate[systemddir]}/watchgate.service
   ${Watchgate[sudo]} ${Watchgate[rm]} -f ${Watchgate[systemddir]}/watchgate.timer
-  ${Watchgate[sudo]} ${Watchgate[rm]} -f ${Watchgate[systemddir]}/timers.target.wants/watchgate.timer
+  ${Watchgate[sudo]} ${Watchgate[rm]} -f \
+  ${Watchgate[systemddir]}/timers.target.wants/watchgate.timer
   ${Watchgate[sudo]} ${Watchgate[rm]} -f /var/lib/systemd/timers/stamp-watchgate.timer
   ${Watchgate[sudo]} ${Watchgate[rm]} -f ${Watchgate[prefix]}${Watchgate[queryscript]}
   ${Watchgate[sudo]} ${Watchgate[rm]} -f ${Watchgate[prefix]}${Watchgate[cronscript]}
@@ -134,10 +147,12 @@ watchgate.seed()
   local seed="${Watchgate[seedprefix]}_\$(${Watchgate[date]} +"%Y%m%d%H%M%S")"
   local tmpfile=\$(${Watchgate[mktemp]})
   builtin trap "${Watchgate[shred]} -u $tmpfile" SIGHUP SIGTERM SIGINT
-  ${Watchgate[sha512sum]} <<<"\$(${Watchgate[head]} -c 1000 /dev/random|${Watchgate[tr]} '\0' ' ')" |\
+  ${Watchgate[sha512sum]} <<<"\$(${Watchgate[head]} -c 1000 \
+  /dev/random|${Watchgate[tr]} '\0' ' ')" |\
     ${Watchgate[cut]} -d' ' -f1 >\$tmpfile
   builtin declare -x GPG_TTY="\$(${Watchgate[tty]})"
-  ${Watchgate[gpg]} --symmetric --no-verbose --quiet --output \$destdir/\$seed.asc --armor \$tmpfile
+  ${Watchgate[gpg]} --symmetric --no-verbose --quiet \
+  --output \$destdir/\$seed.asc --armor \$tmpfile
   ${Watchgate[shred]} -fu \$tmpfile
   ${Watchgate[chmod]} 0400 \$destdir/\$seed.asc
 }
@@ -165,15 +180,21 @@ watchgate.seed.install()
   ${Watchgate[sudo]} ${Watchgate[chown]} root:users \$destseed.asc
   ${Watchgate[sudo]} ${Watchgate[chmod]} 0400 \$destseed
   ${Watchgate[sudo]} ${Watchgate[chown]} root:root \$destseed
-  ${Watchgate[sudo]} ${Watchgate[ln]} -fs \$destseed ${Watchgate[configdir]}${Watchgate[seedprefix]}
-  ${Watchgate[sudo]} ${Watchgate[ln]} -fs \$destseed.asc ${Watchgate[configdir]}${Watchgate[seedprefix]}.asc
+  ${Watchgate[sudo]} ${Watchgate[ln]} -fs \$destseed \
+  ${Watchgate[configdir]}${Watchgate[seedprefix]}
+  ${Watchgate[sudo]} ${Watchgate[ln]} -fs \$destseed.asc \
+  ${Watchgate[configdir]}${Watchgate[seedprefix]}.asc
 }
 watchgate.seed.uninstall()
 {
-  ${Watchgate[sudo]} ${Watchgate[rm]} -f ${Watchgate[configdir]}${Watchgate[seedprefix]}.asc
-  ${Watchgate[sudo]} ${Watchgate[rm]} -f ${Watchgate[configdir]}${Watchgate[seedprefix]}
-  ${Watchgate[sudo]} ${Watchgate[rm]} -f ${Watchgate[configdir]}${Watchgate[seedprefix]}_*.asc
-  ${Watchgate[sudo]} ${Watchgate[rm]} -f ${Watchgate[configdir]}${Watchgate[seedprefix]}_*
+  ${Watchgate[sudo]} ${Watchgate[rm]} -f \
+  ${Watchgate[configdir]}${Watchgate[seedprefix]}.asc
+  ${Watchgate[sudo]} ${Watchgate[rm]} -f \
+  ${Watchgate[configdir]}${Watchgate[seedprefix]}
+  ${Watchgate[sudo]} ${Watchgate[rm]} -f \
+  ${Watchgate[configdir]}${Watchgate[seedprefix]}_*.asc
+  ${Watchgate[sudo]} ${Watchgate[rm]} -f \
+  ${Watchgate[configdir]}${Watchgate[seedprefix]}_*
 }
 watchgate.enable()
 {
